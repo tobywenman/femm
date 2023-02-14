a0e4405850eb19f115341e3394a4719a2aa47f3e
tobywenman <107770465+tobywenman@users.noreply.github.com>
2023-02-14 15:28:58 +0000

    add button and all input boxes

--- week16/designTool.py
+++ week16/designTool.py
@@ -3,13 +3,38 @@ import re
 
 class app(tk.Tk):
 
+    inputs = ["Torque T", 
+              "Magnetic Loading B",
+              "Electrical Loading Q",
+              "Current Density J",
+              "Design flux Density Bmax",
+              "Number of pole pairs p"
+              "Number of slots q",
+              "Outer diameter Do"]
+
     def __init__(self):
         super().__init__()
+
+        #add title
         label = tk.Label(text="3 Phase motor design utility")
         label.pack()
+
+        #setup input boxes
         vcmd = (self.register(self.inputValidation), '%P') #used to update the validation before it's entered into box
-        entry = tk.Entry(self,width=50,validate="key",validatecommand=vcmd)
-        entry.pack()
+        self.entries = []
+        for i in self.inputs:
+            label = tk.Label(text=i)
+            label.pack()
+            entry = tk.Entry(self,width=50,validate="key",validatecommand=vcmd)
+            entry.pack()            
+            self.entries.append(entry)
+
+        button = tk.Button(text="Compute solution",command=self.enterValues)
+        button.pack()
+
+    def enterValues(self):
+        for i in self.entries:
+            print(i.get())
 
     def inputValidation(window,newStr):
         return re.search(r"^$|^\d+[.]?\d*$",newStr) != None
