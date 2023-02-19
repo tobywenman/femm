2c1ddd031a89c80dd7cd4d45c2948c5952f784e0
Toby <tobywenman@gmail.com>
2023-02-19 11:34:48 +0000

    Added output section

--- week16/designTool.py
+++ week16/designTool.py
@@ -8,29 +8,49 @@ class app(tk.Tk):
               "Electrical Loading Q",
               "Current Density J",
               "Design flux Density Bmax",
-              "Number of pole pairs p"
+              "Number of pole pairs p",
               "Number of slots q",
               "Outer diameter Do"]
+
+    outputs = ["Split ratio D/Do",
+               "Active Diameter D",
+               "Active Length L",
+               "Tooth width WT",
+               "Back iron depth dB",
+               "Slot Area As",
+               "Slot bottom dia. Ds",
+               "Outer diameter Do"]
 
     def __init__(self):
         super().__init__()
 
         #add title
         label = tk.Label(text="3 Phase motor design utility")
-        label.pack()
+        label.grid(column=0,columnspan=2)
 
         #setup input boxes
         vcmd = (self.register(self.inputValidation), '%P') #used to update the validation before it's entered into box
         self.entries = []
         for i in self.inputs:
             label = tk.Label(text=i)
-            label.pack()
-            entry = tk.Entry(self,width=50,validate="key",validatecommand=vcmd)
-            entry.pack()            
+            label.grid(column=0)
+            entry = tk.Entry(self,validate="key",validatecommand=vcmd)
+            entry.grid(column=0)          
             self.entries.append(entry)
 
+        self.results = []
+        row = 1
+        for i in self.outputs:
+            label = tk.Label(text=i)
+            label.grid(column=1,row=row)
+            row += 1
+            entry = tk.Entry(self,state="disabled")
+            entry.grid(column=1,row=row)
+            row += 1          
+            self.results.append(entry)
+
         button = tk.Button(text="Compute solution",command=self.enterValues)
-        button.pack()
+        button.grid(column=0,columnspan=2) 
 
     def enterValues(self):
         for i in self.entries:
