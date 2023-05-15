5b4b613409f10f190fa027b905fb3e94eedeec37
Toby <tobywenman@gmail.com>
2023-05-15 10:25:42 +0100

    curved lines

--- week16/femmModel.py
+++ week16/femmModel.py
@@ -31,33 +31,6 @@ class motor:
     def polarToCart(self,theta,D):
         return D*math.cos(math.radians(theta)),D*math.sin(math.radians(theta))
 
-
-    def drawTeeth(self,q,WT,dB,p,D,Do,W1,d1):
-        startAngle = (180-(360/(p/2)))/2
-
-        slotPitch = 360/q
-        gapAngle = math.degrees(W1/D)
-
-        self.drawArc(startAngle,(slotPitch/2)-(gapAngle/2),D)
-
-        p1 = self.polarToCart(startAngle+(slotPitch/2),D)
-        p2 = self.polarToCart(startAngle+(slotPitch/2),Do)
-
-        toothVector = np.array([p2[0]-p1[0],p2[1]-p1[1]])
-
-        toothVector = toothVector / np.linalg.norm(toothVector)
-        toothVector = toothVector * d1
-
-        perpVector = np.array([1,-toothVector[0]/toothVector[1]])
-        perpVector = perpVector / np.linalg.norm(perpVector) 
-        perpVector = perpVector * (W1/2)
-
-        toothVector = toothVector+perpVector
-
-        mi_addnode(p1[0]+toothVector[0],p1[1]+toothVector[1])
-        p3 = self.polarToCart(startAngle+(slotPitch/2)-(gapAngle/2),D)
-        mi_addsegment(p3[0],p3[1],p1[0]+toothVector[0],p1[1]+toothVector[1])
-
     def testTeeth(self,W1,D,d1,q,WT,wTheta,Do,dB,p):
         Ds = Do-dB
         wTheta = math.radians(wTheta)
@@ -67,41 +40,46 @@ class motor:
         b = a * (math.sin(slotPitch/2)/math.sin(wTheta-slotPitch/2))
 
         points = []
+        lines = []
 
-        points.append(np.matrix((W1/2,math.cos(beta)*D/2)))
+        points.append([])
 
-        points.append(np.matrix((W1/2,d1+math.cos(beta)*D/2)))
-        points.append(np.matrix(((W1/2)+b*math.sin(wTheta),math.cos(beta)*D/2+d1+b*math.cos(wTheta))))
-        points.append(np.matrix((math.sin(slotPitch/2)*math.sqrt(Ds**2-WT**2)/2-math.cos(slotPitch/2)*WT/2,math.cos(slotPitch/2)*math.sqrt(Ds**2-WT**2)/2+math.sin(slotPitch/2)*WT/2)))
+        points[0].append(np.matrix((W1/2,math.cos(beta)*D/2)))
 
+        points[0].append(np.matrix((W1/2,d1+math.cos(beta)*D/2)))
+        points[0].append(np.matrix(((W1/2)+b*math.sin(wTheta),math.cos(beta)*D/2+d1+b*math.cos(wTheta))))
+        points[0].append(np.matrix((math.sin(slotPitch/2)*math.sqrt(Ds**2-WT**2)/2-math.cos(slotPitch/2)*WT/2,math.cos(slotPitch/2)*math.sqrt(Ds**2-WT**2)/2+math.sin(slotPitch/2)*WT/2)))
 
+        for i in reversed(points[0]):
+            points[0].append(i*np.matrix([[-1,0],[0,1]]))
 
-        for i in reversed(points):
-            points.append(i*np.matrix([[-1,0],[0,1]]))
+        for i in range(len(points[0])-1):
+            lines.append(line([points[0][i],points[0][i+1]]))
 
         slotPitch = math.pi*2/q
 
-        pointsLen = len(points)
+        pointsLen = len(points[0])
         for i in range(1,q//p):
+            points.append([])
             for j in range(pointsLen):
                 angle = -i*slotPitch
                 rotMatrix = np.matrix([[math.cos(angle),-math.sin(angle)],
                                        [math.sin(angle),math.cos(angle)]])
-                points.append(points[j]*rotMatrix)
-       
-        mi_addnode(points[0][0,0],points[0][0,1])
-        prevPoint = points[0]
+                points[i].append(points[0][j]*rotMatrix)
+            
+            for j in range(len(points[i])-1):
+                lines.append(line([points[i][j],points[i][j+1]]))
 
-        for i in points[1:]:
-            mi_addnode(i[0,0],i[0,1])
-            mi_addsegment(i[0,0],i[0,1],prevPoint[0,0],prevPoint[0,1])
-            prevPoint = i
+        for i in range(len(points)-1):
+            lines.append(line([points[i][-1],points[i+1][0]],curve=True))
 
+        for teeth in points:
+            for i in teeth:
+                mi_addnode(i[0,0],i[0,1])
         
-    
+        for i in lines:
+            i.draw()
 
-
-        
         
     
     
@@ -120,9 +98,36 @@ class motor:
         self.drawTeeth(q,WT,dB,p,D,Do,W1,d1)
         self.drawAirGap(g,D,p)
 
-    
-        
+class line:
 
+    curve = False
+
+    points = [np.matrix([0,0]),np.matrix([0,0])]
+
+    def __init__(self, points=None, curve=None):
+        if curve != None:
+            self.curve = curve
+        if points != None:
+            self.points = points
+    
+    def draw(self, centre=np.matrix([0,0])):
+        if self.curve:
+            print(self.points)
+            vector1 = self.points[0]-centre
+            vector2 = self.points[1]-centre
+
+            vector1 /= np.linalg.norm(vector1)
+            vector2 /= np.linalg.norm(vector2)
+
+            dot = 0
+            for i in [0,1]:
+                dot += vector1[0,i]*vector2[0,i]
+
+            angle = math.degrees(math.acos(dot))
+            mi_addarc(self.points[0][0,0],self.points[0][0,1],self.points[1][0,0],self.points[1][0,1],angle,1)
+        else:
+            mi_addsegment(self.points[0][0,0],self.points[0][0,1],self.points[1][0,0],self.points[1][0,1])
+        
 
 
 
@@ -130,6 +135,6 @@ class motor:
 if __name__=="__main__":
 
     newMotor = motor()
-    newMotor.testTeeth(3.3,129,1,24,9.6,70,200,9.2,4)
+    newMotor.testTeeth(3.3,129,1,24,9.6,70,200,9.2,1)
     
     input("press enter to exit")
